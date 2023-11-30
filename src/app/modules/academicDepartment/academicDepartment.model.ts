7aedd8f77bbd7e9d155e7ea486d95bd9a1b25b5a
Manik <tonysins43@gmail.com>
2023-11-30 15:31:29 +0600

    validate faculty before create and update faculty

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -18,6 +18,26 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+// create pre method for checking existing department for create department
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExists = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExists) {
+    throw new Error('This Department already exists');
+  }
+  next();
+});
+
+// create pre method for checking existing department for update department
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExists = await AcademicDepartment.findOne(query);
+  if (!isDepartmentExists) {
+    throw new Error('This department does not exists');
+  }
+  next();
+});
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -13,6 +13,26 @@ const academicFacultySchema = new Schema<TAcademicFaculty>(
     timestamps: true,
   },
 );
+// create pre method for checking existing faculty for create faculty
+academicFacultySchema.pre('save', async function (next) {
+  const isDepartmentExists = await AcademicFaculty.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExists) {
+    throw new Error('This Faculty already exists');
+  }
+  next();
+});
+
+// create pre method for checking existing faculty for update faculty
+academicFacultySchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExists = await AcademicFaculty.findOne(query);
+  if (!isDepartmentExists) {
+    throw new Error('This Faculty does not exists');
+  }
+  next();
+});
 
 export const AcademicFaculty = model<TAcademicFaculty>(
   'AcademicFaculty',
