33beb1436e3128907dcaf883ee930b80a78a7c37
Manik <tonysins43@gmail.com>
2024-02-01 00:05:50 +0600

    some changes in student routes

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -3,6 +3,7 @@ import { StudentControllers } from './student.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { updateStudentValidationSchema } from './student.validation';
 import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
@@ -10,13 +11,18 @@ const router = express.Router();
 router.get('/', StudentControllers.getAllStudents);
 router.get(
   '/:id',
-  auth('admin', 'faculty', 'student'),
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin, USER_ROLE.faculty),
   StudentControllers.getSingleStudent,
 );
 router.patch(
   '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(updateStudentValidationSchema),
   StudentControllers.updateStudent,
 );
-router.delete('/:id', StudentControllers.deleteSingleStudent);
+router.delete(
+  '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+  StudentControllers.deleteSingleStudent,
+);
 export const StudentRoutes = router;
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -23,7 +23,7 @@ router.post(
 );
 router.post(
   '/create-faculty',
-  auth(USER_ROLE.admin),
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
     req.body = JSON.parse(req.body.data);
@@ -35,6 +35,7 @@ router.post(
 
 router.post(
   '/create-admin',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
     req.body = JSON.parse(req.body.data);
@@ -45,10 +46,19 @@ router.post(
 );
 router.post(
   '/change-status/:id',
-  auth('admin'),
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(UserValidations.changeStatusValidationSchema),
   UserController.changeUserStatus,
 );
-router.get('/me', auth('student', 'faculty', 'admin'), UserController.getMe);
+router.get(
+  '/me',
+  auth(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  UserController.getMe,
+);
 
 export const UserRoutes = router;
